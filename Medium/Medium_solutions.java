42d42a4e8431990199e1fecd69080086ceb1887f
Xavier Maroñas <xmborras@users.noreply.github.com>
2023-02-21 09:32:43 +0100

    Update Medium_solutions.java

--- Medium/Medium_solutions.java
+++ Medium/Medium_solutions.java
@@ -158,3 +158,45 @@ public static void bubbleSort(int[] arr) {
         }
     }
 }
+
+/*
+* Write a program to convert an integer into a binary
+*
+* SOLUTION
+*/
+public class GFG {
+	// Function to print binary number
+	static void printBinary(int[] binary, int id)
+	{
+		// Iteration over array
+		for (int i = id - 1; i >= 0; i--)
+			System.out.print(binary[i] + "");
+	}
+
+	// Function converting decimal to binary
+	public static void decimalToBinary(int num)
+	{
+		// Creating and assigning binary array size
+		int[] binary = new int[35];
+		int id = 0;
+
+		// Number should be positive
+		while (num > 0) {
+			binary[id++] = num % 2;
+			num = num / 2;
+		}
+
+		// Print Binary
+		printBinary(binary, id);
+	}
+
+	// Main Driver Code
+	public static void main(String[] args)
+	{
+		// Entered number to be convert into binary
+		int num = 45;
+
+		// Calling Our Above Function
+		decimalToBinary(num);
+	}
+}
